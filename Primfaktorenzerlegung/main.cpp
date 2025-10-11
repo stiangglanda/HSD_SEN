c38368ad6e1aea445c7684d5706838890f7bed36
Leander Kieweg <kieweg.leander@gmail.com>
2025-10-11 20:20:46 +0200

    remove IsPrime and GetNextPrime functions; refactor PrintResult logic for direct output

--- Primfaktorenzerlegung/main.cpp
+++ Primfaktorenzerlegung/main.cpp
@@ -1,38 +1,6 @@
 #include <iostream>
-#include <vector>
-
-bool IsPrime(const int potentialPrime) {
-    if (potentialPrime<2)
-        return false;
-
-    for (int i = 2; i < potentialPrime; i++)// clamp(potentialPrime/2)
-    {
-        if (potentialPrime % i == 0)
-            return false;
-    }
-    return true;
-}
-
-int GetNextPrime(const int startValue) {
-    for (int i = startValue; true; i++) {
-        if (IsPrime(i))
-            return i;
-    }
-}
-
-void PrintResult(const std::vector<int> &arrayToPrint) {
-    std::cout << "Ausgabe: ";
-    for (int i = 0; i < arrayToPrint.size(); i++) {
-        std::cout << arrayToPrint[i];
-
-        if (i != arrayToPrint.size()-1) {
-            std::cout << ", ";
-        }
-    }
-}
 
 int main() {
-    std::vector<int> primeNumbers;
     int input=0;
 
     std::cout << "Eingabe: ";
@@ -47,19 +15,22 @@ int main() {
     int primeDivisionResult=input;
     int currentPrimeNumber=2;
 
+    std::cout << "Ausgabe: ";
+
     while (primeDivisionResult>1)
     {
         if (primeDivisionResult%currentPrimeNumber==0)
         {
             primeDivisionResult=primeDivisionResult/currentPrimeNumber;
-            primeNumbers.push_back(currentPrimeNumber);
+            std::cout << currentPrimeNumber;
+            if (primeDivisionResult>1) {
+                std::cout << ", ";
+            }
         }
         else {
             currentPrimeNumber++;
-            //currentPrimeNumber=GetNextPrime(currentPrimeNumber+1);
         }
     }
 
-    PrintResult(primeNumbers);
     return 0;
 }
