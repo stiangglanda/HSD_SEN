fafe75512d34635df9453fd9fc7f0399273ed1af
Leander Kieweg <kieweg.leander@gmail.com>
2025-10-11 18:13:12 +0200

    add initial project files and CMake configuration for Primfaktorenzerlegung

---
+++ Primfaktorenzerlegung/main.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+
+bool IsPrime(const int potentialPrime) {
+    for (int i = 2; i < potentialPrime; i++)// clamp(potentialPrime/2)
+    {
+        if (potentialPrime % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int GetNextSmallestPrime(const int startValue) {
+    for (int i = startValue; i >= 2; i--) {
+        if (IsPrime(i))
+            return i;
+    }
+    return 0;
+}
+
+int MultiplyArray(const std::vector<int> &arrayToMultiply) {
+    if (arrayToMultiply.empty()) {
+        return 0;
+    }
+    int result=arrayToMultiply[0];
+
+    for (int i = 1; i < arrayToMultiply.size(); i++) {
+        result*=arrayToMultiply[i];
+    }
+
+    return result;
+}
+
+void PrintResult(const std::vector<int> &arrayToPrint) {
+    std::cout << "Ausgabe: ";
+    for (int i = 0; i < arrayToPrint.size(); i++) {
+        if (i == arrayToPrint.size() - 1) {
+            std::cout << arrayToPrint[i];
+        }
+        else {
+            std::cout << arrayToPrint[i]<<", ";
+        }
+    }
+}
+
+int main() {
+    std::vector<int> primeNumbers={};
+    int input=0;
+
+    std::cout << "Eingabe: ";
+    std::cin >> input;
+
+
+    int primeDivisionResult=input;
+
+    int currentPrimeNumber=GetNextSmallestPrime(input);
+
+    do
+    {
+        if (primeDivisionResult%currentPrimeNumber==0)
+        {
+            primeDivisionResult=primeDivisionResult/currentPrimeNumber;
+            primeNumbers.push_back(currentPrimeNumber);
+        }
+    } while (MultiplyArray(primeNumbers)==input);
+
+    PrintResult(primeNumbers);
+    return 0;
+}
