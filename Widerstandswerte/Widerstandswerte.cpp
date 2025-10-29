25638fa48827a66fa886bb95d755e5e1db6613b4
stiangglanda® <69088823+stiangglanda@users.noreply.github.com>
2025-10-29 22:01:06 +0100

    Add new CMake projects and update build configuration
    Introduced two CMake projects: `Primfaktorenzerlegung` and
    `Widerstandswerte`. Updated `CMakeLists.txt` to define project
    settings, including C++20 standard and MSVC Hot Reload support.
    
    Added a comprehensive `.gitignore` to exclude Visual Studio-specific
    files, build artifacts, and other temporary files.
    
    Created `CMakePresets.json` to streamline build configurations with
    presets for `x64` and `x86` architectures in both Debug and Release
    modes.
    
    Added `Widerstandswerte.cpp` as the entry point for the
    `Widerstandswerte` project, with a simple "Hello CMake." output.

---
+++ Widerstandswerte/Widerstandswerte.cpp
@@ -0,0 +1,11 @@
+﻿// Widerstandswerte.cpp : Defines the entry point for the application.
+//
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
