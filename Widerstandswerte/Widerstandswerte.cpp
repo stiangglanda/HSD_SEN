cf59cf30f4880105dacedf8f591cfbf04278785a
Leander Kieweg <kieweg.leander@gmail.com>
2025-11-01 16:16:39 +0100

    Refactor resistance value handling and improve error messages

--- Widerstandswerte/Widerstandswerte.cpp
+++ Widerstandswerte/Widerstandswerte.cpp
@@ -10,45 +10,45 @@ int main()
 {
 	const unsigned int rangeMin = 10;
 	const unsigned int rangeMax = 5000000;
-	const unsigned int RingCount = 2;
-	unsigned int multiplikatorZeroCount=0;
-	string ResistenceColors[] = {"schwarz", "braun",
-		"rot","orange", "gelb", "grün",
-		"blau", "violett", "grau", "weiß"};
-	unsigned int ResistenceValue=0;
-	vector<int> ResistorRingsVec;
+	const unsigned int ringCount = 2;
+	const string resistorColors[] = {"schwarz", "braun",
+	"rot","orange", "gelb", "grün",
+	"blau", "violett", "grau", "weiß"};
+	unsigned int multiplierZeroCount=0;
+	unsigned int resistenceValue=0;
+	vector<unsigned int> resistenceDigitVec;
 
-	cout << "Enter the Resistence Value in Ohm between Min: "<<rangeMin<<" and Max: "<<rangeMax << endl;
-	cin >> ResistenceValue;
+	cout << "Eingabe von einem Wiederstandswert in Ohm zwischen: " << rangeMin << " und " << rangeMax << endl;
+	cin >> resistenceValue;
 
-	if (ResistenceValue < rangeMin || ResistenceValue > rangeMax) {
-		cout << "Resistence Value is out of bounds: Min: "<<rangeMin<<", Max: "<< rangeMax << endl;
+	if (resistenceValue < rangeMin || resistenceValue > rangeMax) {
+		cerr << "Resistence Value is out of bounds: Min: "<<rangeMin<<", Max: "<< rangeMax << endl;
 		return 1;
 	}
 
-	while(ResistenceValue>0) {
-		ResistorRingsVec.insert(ResistorRingsVec.begin(),ResistenceValue%10);//insert so you dont need invert
-		ResistenceValue/=10;
+	while(resistenceValue>0) {
+		resistenceDigitVec.insert(resistenceDigitVec.begin(),resistenceValue%10);//insert so you dont need invert
+		resistenceValue/=10;
 	}
 
-	for (int i=0;i<ResistorRingsVec.size();i++) {
-		if (i<RingCount) {//Ring
-			cout << ResistenceColors[ResistorRingsVec[i]];
-			if (i < RingCount - 1) { // Nur nach dem ersten Ring einen Bindestrich setzen
+	for (int i=0;i<resistenceDigitVec.size();i++) {
+		if (i<ringCount) {//Ring
+			cout << resistorColors[resistenceDigitVec[i]];
+			if (i < ringCount - 1) {
 				cout << "-";
 			}
 		}
-		else {//Multiplikator
-			if (ResistorRingsVec[i]==0) {
-				multiplikatorZeroCount++;
+		else {//Multiplier
+			if (resistenceDigitVec[i]==0) {
+				multiplierZeroCount++;
 			}
 			else {
-				cout << endl <<"ERROR Multiplikator is wrong"<<endl;
+				cerr << endl <<"ERROR Multiplikator ist Falsch"<<endl;
 				return 1;
 			}
 		}
 	}
-	cout << "-" << ResistenceColors[multiplikatorZeroCount];
+	cout << "-" << resistorColors[multiplierZeroCount];
 
 	return 0;
 }
