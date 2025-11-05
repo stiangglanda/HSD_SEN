c235cc0b6a762da7851abe1bd727eb4a543eacbf
Leander Kieweg <kieweg.leander@gmail.com>
2025-11-05 16:56:43 +0100

    Refactor average consumption calculation to use double type and improve input validation

--- Durchschnittsverbrauch/main.cpp
+++ Durchschnittsverbrauch/main.cpp
@@ -3,20 +3,23 @@
 using namespace std;
 
 // Gibt den Durchnscnittsverbrauch pro 1/100 km zurück bei fehler giebt es -1 zurück
-float Durchnscnittsverbrauch(float Kilometer, float Treibstoffmenge) {
-    if (Kilometer==0) {
+double Durchnscnittsverbrauch(const double& Kilometer, const double& Treibstoffmenge) {// immer wenn daten größer sind const ref nemen
+    if (Kilometer>0&&Treibstoffmenge>0) {
         return -1;
     }
 
-    return ((Treibstoffmenge/Kilometer)/1)*100;
+    return Treibstoffmenge * 100 / Kilometer;
 }
 
 int main() {
-    float Kilometer = 0;
-    float Treibstoffmenge = 0;
+    double Kilometer = 0;
+    double Treibstoffmenge = 0;
 
-    cout << "Kilometer eingabe:" << endl;
-    cin >> Kilometer;
+    while (Kilometer==0) {
+        cout << "Kilometer eingabe:" << endl;
+        cin >> Kilometer;
+    }
+
     cout << "Treibstoffmenge eingabe:" << endl;
     cin >> Treibstoffmenge;
 
