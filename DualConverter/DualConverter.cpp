8c3db9327763976f8661cd2f66f4dd56c6d9fa6e
Leander Kieweg <kieweg.leander@gmail.com>
2025-11-16 02:43:52 +0100

    Refactor Dual2Dec function to use unsigned int and improve error handling for invalid binary input

--- DualConverter/DualConverter.cpp
+++ DualConverter/DualConverter.cpp
@@ -11,9 +11,10 @@
 
 using namespace std;
 
-int Dual2Dec(bool a, bool b, bool c, bool d, bool e, bool f, bool g, bool h)
+unsigned int Dual2Dec(bool b7, bool b6, bool b5, bool b4, bool b3, bool b2, bool b1, bool b0)
 {
-    return a * 128 + b * 64 + c * 32 + d * 16 + e * 8 + f * 4 + g * 2 + h * 1;
+    return b7 * 128 + b6 * 64 + b5 * 32 + b4 * 16 +
+           b3 * 8 + b2 * 4 + b1 * 2 + b0 * 1;
 }
 
 int Dual2Dec(size_t dualValue)
@@ -22,6 +23,9 @@ int Dual2Dec(size_t dualValue)
     int position = 0;
     while (dualValue > 0) {
         int lastDigit = dualValue % 10;
+        if (lastDigit > 1) {
+            return 0; // Fehlerbehandlung bei nicht dualzahl
+        }
         decimalValue += lastDigit * (1 << position); // 2^position
         dualValue /= 10;
         position++;
@@ -31,9 +35,10 @@ int Dual2Dec(size_t dualValue)
 
 int main() 
 {
-    size_t DualNumber = 10010010;
+    size_t DualNumber = 11111111;
 
-    cout << "Output: size_t variant: " << Dual2Dec(DualNumber) << " bool variant: " << Dual2Dec(1,0,0,1,0,0,1,0) << endl;
+    cout << "Output: size_t variant: " << Dual2Dec(DualNumber)
+    << " bool variant: " << Dual2Dec(1,1,1,1,1,1,1,1) << endl;
 
     return 0;
 }
