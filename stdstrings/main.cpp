672f6eab7ac57575521a8be578e83196ed30e7bc
Leander Kieweg <kieweg.leander@gmail.com>
2025-11-25 11:07:31 +0100

    Add string manipulation examples and comments in main.cpp

--- stdstrings/main.cpp
+++ stdstrings/main.cpp
@@ -21,5 +21,82 @@ int main() {
 
     output=str1+str2+str3;
     cout << output << endl;
+
+    output=output+" von ";
+    output+="Maxi.";
+    cout << output << endl;
+
+    // Laenge einer Zeichenkette
+    // -------------------------
+    size_t len = output.size();// = length()
+    cout << "Stringlaenge: " << len << endl;
+
+    // String leeren
+    // -------------
+
+    output.erase(); // oder: output = "";
+    len = output.size();// = length()
+    cout << "Stringlaenge: " << len << endl;
+
+    // Vergleich (case sensitive)
+    // --------------------------
+    if (str3=="Textzeile") {        //analog mit Operator !=
+        cout << "identisch" << endl;
+    }
+    else {
+        cout << "Verschieden" << endl;
+    }
+
+    // Initialisierung
+    // ---------------
+    string str=""; //Leerstring
+    string fiveStars(5,'*'); //Initialisierung mit 5 Sternen
+    cout << fiveStars << endl;
+
+    // indizierter Zugriff mit []
+    // --------------------------
+    str = "Hallo";
+    len = str.size();
+
+    //Range: str[0]..str[len-1]     .. wie bei Feldern
+    char ch = str[0]; // ch='H'
+    for (size_t i = 0; i < len; i++) { // kompletten string zeichenweise ausgeben
+        cout << str[i];
+    }
+    cout << endl;
+    str[0]='R';
+    str[1]='o';
+    cout << str << endl; // "Rollo"
+
+    // Hinweis: Mit dem Indizierungsoperator [] lassen sich keine zusaetzlichen Zeichen
+    //          hinten am String anfuegen! Dafuer ist insert, + oder += zu verwenden!
+
+    // Teilstring ermitteln
+    // --------------------
+    string s="A Test String.";
+    string sub=s.substr(2,4); // substr(Position, Anzahl Zeichen)
+    cout << "Substring: " << sub << endl; // "Test"
+
+    // Ersetzen eines Teilbereichs im String
+    // -------------------------------------
+    s.replace(2,4,"simple");    // replace(Position, Anzahl Zeichen, neuer String)
+    cout << s << endl;
+
+    // Einfuegen
+    // ---------
+    s.insert(9,"C++"); // insert(Position, string)
+    cout << s << endl;
+
+    // Suchen im String
+    // ----------------
+    size_t pos = s.find("C++", 0);  //find(substring, Startposition)
+
+    if (pos==s.npos) {
+        cout << "nicht enthalten" << endl;
+    }
+    else {
+        cout << "Fefunden an Index " << pos << endl;
+    }
+
     return 0;
 }
