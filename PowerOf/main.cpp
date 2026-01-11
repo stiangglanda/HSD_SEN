dae468b56ef3387b31108c38f4a7d80e72fe8906
Leander Kieweg <kieweg.leander@gmail.com>
2026-01-11 04:20:45 +0100

    Add IsPowerOf recursive function and improve output formatting

--- PowerOf/main.cpp
+++ PowerOf/main.cpp
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////
+// Workfile : main.cpp
+// Author : Leander Kieweg
+// Date : 10. 01. 2026
+// Description : IsPowerOf Recursive Funktion
+// Remarks : -
+// Revision : 0
+///////////////////////////////////////////////////////////////////////////
+
 #include <iostream>
 
 bool IsPowerOf(int const x, int const n) {
@@ -5,7 +14,7 @@ bool IsPowerOf(int const x, int const n) {
         return false;
     } else if (n == 1) {
         return x == 1;
-    } else if (x == 1 || x == -1) {
+    } else if (x == 1) {
         return true;
     } else if (x%n != 0) {
         return false;
@@ -17,6 +26,6 @@ bool IsPowerOf(int const x, int const n) {
 int main() {
     int const x = 128;
     int const n = 2;
-    std::cout << "IsPowerOf(" << x<< "," << n << "): " << std::boolalpha << IsPowerOf(x,n) << std::endl;
+    std::cout << "IsPowerOf(" << x << "," << n << "): " << std::boolalpha << IsPowerOf(x,n) << std::endl;
     return 0;
 }
