57b1f65878cc5c4b2341d2ad35df8483db7b176d
Leander Kieweg <kieweg.leander@gmail.com>
2026-03-03 09:38:17 +0100

    Add new classes and update main implementation with additional functionality

--- BinomealBerechnung/faculty.h
+++
@@ -1,6 +0,0 @@
-#ifndef FACULTY_INCLUDED
-#define FACULTY_INCLUDED
-
-int Faculty(int x);
-
-#endif
--- RandomNumberLaeufeTest/RandomGen.cpp
+++
@@ -1,8 +0,0 @@
-#include <ctime>
-#include <random>
-#include "RandomGen.h"
-
-int rgen::GetRandVal(int const lo, int const hi) {
-    static std::mt19937_64 generator((unsigned)std::time(0));
-    return std::uniform_int_distribution <int>(lo, hi) (generator);
-}
