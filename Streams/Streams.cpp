c550b852e4ced5a64622e213bf020ed0b5ab4336
Leander Kieweg <leander.kieweg@gmail.com>
2026-03-11 13:01:59 +0100

    Add project configuration files and initial implementation for Streams; set up .gitignore and editor settings

--- Streams/Streams.cpp
+++ Streams/Streams.cpp
@@ -82,6 +82,40 @@ int main()
 
    // to do
 
+   if (CheckFiles(binIn, binOut, cErrBinCopy)) {
+      binOut << binIn.rdbuf(); // kopiert die Daten direkt aus dem Datenpuffer und schreibt diese auf binOut
+
+      if (!CheckInFile(binIn)) {
+         binOut.close();
+         return 1;
+      }
+
+      string const cBuffer = "********END OF FILE*********";
+      //binOut.seekp(100);
+
+      //Variante 1
+      //binOut.seekp(0);
+      //Variante 2
+      //binOut.seekp(-10, ios::beg);
+      //Variante 3
+      binOut.seekp(100,ios::end);
+      // schreiben mit der write-Funktion
+      binOut.write(cBuffer.c_str(), cBuffer.size());
+
+      if (!CheckOutFile(binOut)) {
+         binIn.close();
+         return 1;
+      }
+
+      //Dateigroesse bestimmen
+      binOut.seekp(0,ios::end); //->Dateiende
+      cout << "Groesse: " << binOut.tellp() << " Bytes" << endl;
+      binIn.seekg(0,ios::end); //->Dateiende
+      cout << "Groesse: " << binIn.tellg() << " Bytes" << endl;
+
+      binIn.close();
+      binOut.close();
+   }
 
    // Textdatei kopieren - Datei wird Zeichen fuer Zeichen gelesen
    // Die Datei wird geoeffnet und gelesen, indem ein Eingabe-Dateistrom erzeugt wird.
@@ -89,4 +123,27 @@ int main()
    ofstream outfile(cFileNameDupText);
 
    // to do
+   if (CheckFiles(infile, outfile, cErrTextCopy)) {
+      char ch = -1;
+      while (outfile.good() && infile.get(ch)) {
+         cout << ch;
+         outfile.put(ch);
+         outfile << '_';
+      }
+
+      //Lesefehler pruefen
+      if (!CheckInFile(infile)) {
+         outfile.close();
+         return 1;
+      }
+
+      //Schreibfehler pruefen
+      if (!CheckOutFile(outfile)) {
+         infile.close();
+         return 1;
+      }
+
+      outfile.close();
+      infile.close();
+   }
 }
