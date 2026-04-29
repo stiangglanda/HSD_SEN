4cc0793756dbda183622d7e6fb7d09eae37d914f
Leander Kieweg <leander.kieweg@gmail.com>
2026-04-29 12:39:17 +0200

    Add STL algorithm implementations and project configuration files

--- STLAlgos/algos.cpp
+++ STLAlgos/algos.cpp
@@ -14,7 +14,42 @@ void Test_Algos(string const& header)
       TDeqCont cont(5);  //Container mit Speicher für 5 Elemente
    
       //to do
+      fill(cont.begin(), cont.end(), 1);
+      cout << cont;
 
+      fill_n(cont.end() - 2, 2, 4);
+      cout << cont;
+
+      cont.push_back(5);
+      copy_backward(cont.cend()-2, cont.cend(), cont.begin()+2);
+      cout << cont;
+
+      reverse_copy(cont.cend()-2, cont.cend(), cont.begin());
+      cout << cont;
+
+      cout << "sum: " << accumulate(cont.cbegin(), cont.cend(), TDeqCont::value_type{} /*0*/ /*int{}*/) << endl;
+
+      sort(cont.begin(), cont.end());
+      cout << boolalpha << binary_search(cont.cbegin(), cont.cend(), 1) << endl;
+
+      auto lb = lower_bound(cont.cbegin(), cont.cend(), 4);
+      auto ub = upper_bound(cont.cbegin(), cont.cend(), 4);
+      if (lb != cont.cend() && ub != cont.cend()) {
+         cout << cont;
+         cout << "lb at: " << distance(cont.cbegin(), lb) << endl;
+         cout << "ub at: " << distance(cont.cbegin(), ub) << endl;
+      }
+
+      replace(cont.begin(), cont.end(), 4, 4711);
+      cout << cont;
+
+      auto end = remove(cont.begin(), cont.end(), 4711);
+      cont.erase(end, cont.cend());
+      cout << cont;
+
+      end = unique(cont.begin(), cont.end());
+      cont.erase(end, cont.cend());
+      cout << cont;
    }
    catch (std::bad_alloc const& ex)
    {
