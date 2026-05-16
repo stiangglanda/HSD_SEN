4b24149d47620258e92513592d0be505d2fe2e52
Leander Kieweg <leander.kieweg@gmail.com>
2026-05-16 18:21:41 +0200

    Refactor main.cpp to modularize test cases and enhance vector operations with runTestCase function

--- MengenOperationen/main.cpp
+++ MengenOperationen/main.cpp
@@ -16,31 +16,60 @@ void printVector(const std::string& prefix, const std::vector<int>& vec) {
     std::cout << std::endl;
 }
 
-int main() {
-    std::vector<int> vec1{ 8, 4, 3, 2, 3, 1, 8 };
-    std::vector<int> vec2{ 3, 7, 1, 6, 5, 7 };
-
+void runTestCase(const std::string& name, const std::vector<int>& vec1, const std::vector<int>& vec2, size_t u_size, size_t i_size, size_t d_size) {
+    std::cout << "--- " << name << " ---\n";
     printVector("Container 1", vec1);
     printVector("Container 2", vec2);
+
+    std::vector<int> u(u_size);
+    std::vector<int> i(i_size);
+    std::vector<int> d(d_size);
+
+    Union(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), u.begin());
+    Intersection(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), i.begin());
+    Difference(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), d.begin());
+
+    printVector("Union", u);
+    printVector("Intersection", i);
+    printVector("Difference", d);
     std::cout << std::endl;
+}
 
-    const size_t Union_size = 8; // Exact size for Union
-    const size_t Intersection_size = 2; // Exact size for Intersection
-    const size_t Difference_size = 3; // Exact size for Difference
+int main() {
+    std::vector<int> baseVec1{ 8, 4, 3, 2, 3, 1, 8 };
+    std::vector<int> baseVec2{ 3, 7, 1, 6, 5, 7 };
+    const size_t base_u = 8;
+    const size_t base_i = 2;
+    const size_t base_d = 3;
+    runTestCase("Base Case", baseVec1, baseVec2, base_u, base_i, base_d);
 
-    // Pre-allocate exact required sizes since the functions return void
-    std::vector<int> vec3(Union_size);
-    std::vector<int> vec4(Intersection_size);
-    std::vector<int> vec5(Difference_size);
+    std::vector<int> emptyVec1{};
+    std::vector<int> emptyVec2{};
+    const size_t empty_both_u = 0;
+    const size_t empty_both_i = 0;
+    const size_t empty_both_d = 0;
+    runTestCase("Beide Mengen Empty", emptyVec1, emptyVec2, empty_both_u, empty_both_i, empty_both_d);
 
-    Union(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), vec3.begin());
-    printVector("Union", vec3);
+    std::vector<int> partialVec1{ 1, 2, 3 };
+    std::vector<int> partialVec2{};
+    const size_t partial_u = 3;
+    const size_t partial_i = 0;
+    const size_t partial_d = 3;
+    runTestCase("Eine Menge Empty", partialVec1, partialVec2, partial_u, partial_i, partial_d);
 
-    Intersection(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), vec4.begin());
-    printVector("Intersection", vec4);
+    std::vector<int> idVec1{ 1, 2, 3 };
+    std::vector<int> idVec2{ 1, 2, 3 };
+    const size_t id_u = 3;
+    const size_t id_i = 3;
+    const size_t id_d = 0;
+    runTestCase("Identische Mengen", idVec1, idVec2, id_u, id_i, id_d);
 
-    Difference(vec1.begin(), vec1.end(), vec2.begin(), vec2.end(), vec5.begin());
-    printVector("Difference", vec5);
+    std::vector<int> disVec1{ 1, 2, 3 };
+    std::vector<int> disVec2{ 4, 5, 6 };
+    const size_t dis_u = 6;
+    const size_t dis_i = 0;
+    const size_t dis_d = 3;
+    runTestCase("Verschiedene Mengen", disVec1, disVec2, dis_u, dis_i, dis_d);
 
     return 0;
 }
