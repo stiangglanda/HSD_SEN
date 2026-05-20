707c609bf93fabd50bf9212e79c67466a9b0f94c
Leander Kieweg <leander.kieweg@gmail.com>
2026-05-20 12:36:17 +0200

    Add functors for arithmetic operations and sum calculation in STLFunctors.cpp

--- Functor/STLFunctor/STLFunctors.cpp
+++ Functor/STLFunctor/STLFunctors.cpp
@@ -19,6 +19,10 @@ using namespace std;
 template<typename T>
 class Plus         
 {
+public:
+   T operator()(T a, T b) const {
+      return a + b;
+   }
    //to do
 };
 
@@ -28,14 +32,28 @@ template<typename T>
 struct Equal_to 
 {
    //to do
+   bool operator()(T const& x, T const& y) const {
+      return x == y;
+   }
 };
 
 
 //Funktor zur Ermittlung und Speicherung einer Summe aller geraden Elemente
 template<typename T>
-class SumEven
-{
- //to do
+class SumEven {
+public:
+   SumEven(T const& startVal = T{}) : mSum{startVal}
+   {}
+
+   void operator()(T const& val) {
+      if (val % 2 == 0) {
+         mSum = mSum + val;
+      }
+   }
+
+   T GetSum() const { return mSum; }
+private:
+   T mSum;
 };
 
 
@@ -62,6 +80,33 @@ int main()
       auto vInt = vector<int>{ 1,2,3,4,5 };
 
       //TO DO
+      //nur zur Demo, wie ein Funktor funktioniert
+      Plus<int> ownPlus;
+      cout << boolalpha << (ownPlus(1,2) == std::plus<int>{}(1,2) )<< endl;
+      cout  << (Plus<string>{}("a", "bc") == std::plus<string>{}("a","b") )<< endl;
+
+      Equal_to<int> cmpInt;
+      cout << cmpInt(1,3) << endl;
+      cout << Equal_to<Fraction>{}(Fraction{1,2},Fraction{1,3}) << endl;
+
+      //Funktor mit STL-Algo verwenden
+      cout << "sum: " << accumulate(vInt.cbegin(), vInt.cend(), int{}, Plus<int>{}) << endl;
+      cout << "sum: " << accumulate(vInt.cbegin(), vInt.cend(), int{}) << endl;
+
+      //Equal_to in transform mit 2 Quellen und ostream als Ziel
+      transform(vInt.cbegin(), vInt.cend(), dInt.cbegin(), ostream_iterator<int>(cout<<endl, " "), Equal_to<int>{});
+
+      //Anwendung SumEven
+      SumEven<int> sum1{4711};
+      for_each(vInt.cbegin(), vInt.cend(), sum1);
+      cout << endl << sum1.GetSum() << endl;
+
+      //besser
+      sum1 = for_each(vInt.cbegin(), vInt.cend(), SumEven<int>{});
+      cout << endl << sum1.GetSum() << endl;
+
+      //noch kompakter
+      cout << "sum: " << for_each(vInt.cbegin(), vInt.cend(), SumEven<int>{4711}).GetSum() << endl;
    }
    catch (bad_alloc const& exc)
    {
