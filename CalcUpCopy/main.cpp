bd795438008cec2dd2e4526cbafc18e5ab39c01f
Leander Kieweg <leander.kieweg@gmail.com>
2026-05-24 17:52:42 +0200

    Refactor main function to use RunTestCase for improved readability and testing of CalcUp_copy

--- CalcUpCopy/main.cpp
+++ CalcUpCopy/main.cpp
@@ -8,9 +8,22 @@
 ///////////////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <functional>
 #include <iterator>
+#include <string>
 #include "CalcUpCopy.h"
+
+template<typename TCont, typename TPred>
+void RunTestCase(const std::string& testName, const TCont& input, TPred op) {
+    std::cout << testName << std::endl;
+    std::cout << "Input: ";
+    std::copy(input.cbegin(), input.cend(), std::ostream_iterator<int>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::cout << "Output: ";
+    CalcUp_copy(input.cbegin(), input.cend(), std::ostream_iterator<int>{std::cout, " "}, op);
+    std::cout << std::endl;
+}
 
 int main()
 {
@@ -18,14 +31,17 @@ int main()
         int result = lhs-rhs;
         return result*result;
     };
-    std::vector<int> vec{1, 2, 3, 4, 5, 8};
-    std::vector<int> vec2{ 100, 200, 300, 400 };
-    std::vector<int> vecRes(3);
-    std::vector<int> vecRes2(2);
-    CalcUp_copy(vec.cbegin(), vec.cend(), vecRes.begin(), std::plus<int>{});
-    CalcUp_copy(vec2.cbegin(), vec2.cend(), vecRes2.begin(), SquareDiff);
 
-    std::copy(vecRes.cbegin(), vecRes.cend(), std::ostream_iterator<int>{std::cout, " "});
-    std::cout << std::endl;
-    std::copy(vecRes2.cbegin(), vecRes2.cend(), std::ostream_iterator<int>{std::cout, " "});
+    RunTestCase("plus<int> (Expected: 9 7 7)", std::vector<int>{1, 2, 3, 4, 5, 8}, std::plus<int>{});
+    RunTestCase("SquareDiff (Expected: 90000 10000)", std::vector<int>{100, 200, 300, 400}, SquareDiff);
+
+    RunTestCase("minus<int>", std::vector<int>{10, 20, 5, 2}, std::minus<int>{});
+    RunTestCase("multiplies<int>", std::vector<int>{2, 3, 4, 5}, std::multiplies<int>{});
+    RunTestCase("divides<int>", std::vector<int>{100, 50, 10, 4}, std::divides<int>{});
+
+    RunTestCase("Empty container", std::vector<int>{}, std::plus<int>{});
+    RunTestCase("Container with 1 element", std::vector<int>{42}, std::plus<int>{});
+    RunTestCase("Odd number of elements (5 elements)", std::vector<int>{1, 2, 3, 4, 5}, std::plus<int>{});
+
+    return 0;
 }
