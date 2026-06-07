ebb09993bb2def3e8c93a90c9977049e9ffce853
Leander Kieweg <leander.kieweg@gmail.com>
2026-06-07 17:38:35 +0200

    Refactor StringStatistic class: use default constructor, improve Print method, and add ostream operator overload

--- StringStatistic/StringStatistic.h
+++ StringStatistic/StringStatistic.h
@@ -1,10 +1,11 @@
+#include <iomanip>
 #include <limits>
 
 class StringStatistic{
 public:
-   StringStatistic() {};
+   StringStatistic() = default;
 
-   void operator() (std::string str) {
+   void operator() (std::string const& str) {
       if(str.size() < mStrMin) {
          mStrMin = str.size();
       }
@@ -15,16 +16,27 @@ public:
       mStrCnt++;
    };
 
-   void Print() {
-      std::cout << "string statistic:" << std::endl;
-      std::cout << "min: " << mStrMin << std::endl;
-      std::cout << "max: " << mStrMax << std::endl;
-      std::cout << "avg: " << static_cast<double>(mStrSizeSum)/static_cast<double>(mStrCnt);
+   void Print(std::ostream& ost = std::cout) const {
+      ost << "string statistic:" << std::endl;
+      if (mStrCnt == 0) {
+         ost << "min: 0" << std::endl << "max: 0" << std::endl << "avg: 0.00" << std::endl;
+         return;
+      }
+
+      ost << "min: " << mStrMin << std::endl
+          << "max: " << mStrMax << std::endl
+          << "avg: " << std::fixed << std::setprecision(2)
+          << (static_cast<double>(mStrSizeSum) / mStrCnt) << std::endl;
    }
 private:
    size_t mStrMin = std::numeric_limits<size_t>::max();
    size_t mStrMax = 0;
-
    size_t mStrCnt = 0;
    size_t mStrSizeSum = 0;
+   const size_t mPrecision = 2;
 };
+
+std::ostream& operator<<(std::ostream& ost, StringStatistic const& stat) {
+   stat.Print(ost);
+   return ost;
+}
