cc146dc23836a849eee5590cc68e6a80549b800d
Leander Kieweg <leander.kieweg@gmail.com>
2026-06-07 18:23:02 +0200

    Refactor main function: extract run_test function for improved readability and add test cases for various input scenarios

--- StringStatistic/main.cpp
+++ StringStatistic/main.cpp
@@ -3,23 +3,38 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-
 #include "StringStatistic.h"
 
-using namespace std;
+void run_test(const std::vector<std::string>& vec, const std::string& test_name) {
+   std::cout << test_name << std::endl;
+   std::cout << "Input strings: ";
+   if (vec.empty()) {
+      std::cout << "(empty)";
+   } else {
+      std::copy(vec.cbegin(), vec.cend(), std::ostream_iterator<std::string>(std::cout, " "));
+   }
+   std::cout << std::endl << std::for_each(vec.cbegin(), vec.cend(), StringStatistic{}) << std::endl;
+}
 
 int main()
 {
-   vector<string> val{"abc", "x", "abcdefgh", "aa"};
-   //Input strings: abc x abcdefgh aa
-   //string statistic:
-   //min: 1
-   //max: 8
-   //avg: 3.50
-   StringStatistic strStat{};
-   for_each(val.cbegin(), val.cend(), [&](string str) { strStat(str); } );
+   std::vector<std::string> val{"abc", "x", "abcdefgh", "aa"};
+   run_test(val, "Standard");
 
-   strStat.Print();
+   std::vector<std::string> empty_val{};
+   run_test(empty_val, "Empty vector");
 
-   cout << endl;
+   std::vector<std::string> empty_strings{"", "", ""};
+   run_test(empty_strings, "Only empty strings");
+
+   std::vector<std::string> single_string{"hello"};
+   run_test(single_string, "Only one string");
+
+   std::vector<std::string> same_length{"123", "456", "789"};
+   run_test(same_length, "Strings of equal length");
+
+   std::vector<std::string> special_chars{"a b", "   ", "!?"};
+   run_test(special_chars, "Strings with spaces and special characters");
+
+   return 0;
 }
